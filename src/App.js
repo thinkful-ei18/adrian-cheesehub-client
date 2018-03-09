e3fddd9ea01f3380083cccbc87ad843d77a1e54a
Adrian <adriantoddross@gmail.com>
2018-03-09 12:24:40 -0500

    render CheeseList, import cheese logo!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CheeseList from './components/cheese-list';
 
 class App extends Component {
+
   render() {
+    const cheeses =   [
+      "Bath Blue",
+      "Barkham Blue",
+      "Buxton Blue"
+    ];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to CheeseList</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CheeseList cheeseList={cheeses}/>
       </div>
     );
   }
