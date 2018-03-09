c1e43553339a3b620c8112f1100bd577d385a598
Adrian <adriantoddross@gmail.com>
2018-03-09 12:30:57 -0500

    stub cheese actions

---
+++ src/actions/cheese.js
@@ -0,0 +1,3 @@
+// In client/src/actions/cheese.js add an async action called fetchCheeses which uses the Fetch API to make a GET request to your /api/cheeses endpoint.
+
+// The async action should dispatch its sync counterparts, fetchCheesesRequest, fetchCheesesSuccess and fetchCheesesError.
