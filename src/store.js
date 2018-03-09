c6625786cf81ed198d93f043cead6d865f40ef91
Adrian <adriantoddross@gmail.com>
2018-03-09 15:29:46 -0500

    tidy code

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {cheeseReducer} from './reducers';
 
-export default createStore(
+export default createStore (
     cheeseReducer,
     applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
