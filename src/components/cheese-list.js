abc836a9f3f271db41357cbe4c4c8aacae8b5972
Adrian <adriantoddross@gmail.com>
2018-03-09 12:18:45 -0500

    return cheeseList

--- src/components/cheese-list.js
+++ src/components/cheese-list.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+export default function CheeseList (props) {
+
+const cheeses = props.cheeseList.map(cheese =>
+  <li className="cheese-list-cheese">
+    {cheese}
+  </li>
+);
+
+return (
+  <div className="cheese-list-wrapper">
+    <ul className="cheese-list">
+      {cheeses}
+    </ul>
+  </div>
+);
+
+}
