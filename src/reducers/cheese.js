19d14cd1ca9e32c8b7d5f22b4fbae36f8b6709a0
Adrian <adriantoddross@gmail.com>
2018-03-09 14:59:10 -0500

    revise reducer

--- src/reducers/cheese.js
+++ src/reducers/cheese.js
@@ -1,4 +1,4 @@
-import {FETCH_CHEESE_ERROR, FETCH_CHEESE_SUCCESS} from '../actions/cheese'
+import {FETCH_CHEESES_ERROR, FETCH_CHEESES_SUCCESS, FETCH_CHEESES_REQUEST} from '../actions/cheese'
 
 const initialState = {
   cheeses: [],
@@ -6,18 +6,36 @@ const initialState = {
   error: null
 }
 
-export default function reducer(state = initialState, action) {
-  if (action.type === FETCH_CHEESE_SUCCESS) {
-      return Object.assign({}, state, {
-          data: action.data,
-          error: null
+export function cheeseReducer(state=initialState, action) {
+
+  if (action.type === FETCH_CHEESES_REQUEST) {
+    return Object.assign({}, state,
+    {
+      loading: true,
+      error: null
+    });
+  }
+  else if (action.type === FETCH_CHEESES_SUCCESS) {
+
+    if (action.cheeses.length < 1) {
+      const error = 'Could not find any cheese!'
+      return Object.assign({}, state,
+      {error: error, loading: false});
+    }
+
+    return Object.assign({}, state,
+      {
+      cheeses: action.data,
+      loading: false,
+      error: null
       });
-  } else if (action.type === FETCH_CHEESE_ERROR) {
-      return Object.assign({}, state, {
-          error: action.error
+  }
+  else if (action.type === FETCH_CHEESES_ERROR) {
+    return Object.assign({}, state,
+      {
+        error: action.error,
+        loading: false
       });
   }
   return state;
 }
-
-
