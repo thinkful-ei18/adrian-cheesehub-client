fa7d14698be4372419630cccc9f3f2b27b5569a0
Adrian <adriantoddross@gmail.com>
2018-03-09 16:37:45 -0500

    import & use redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {cheeseReducer} from './reducers/cheese';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // applyMiddleware(thunk)
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore (
-    cheeseReducer, applyMiddleware(thunk)
+    cheeseReducer, composeWithDevTools(
+        applyMiddleware(thunk),
+    )
 );
