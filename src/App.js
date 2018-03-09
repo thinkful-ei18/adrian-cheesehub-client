ff5ca7aa87dffbf91d8421e34cd4f2afef4d3a64
Adrian <adriantoddross@gmail.com>
2018-03-09 12:26:10 -0500

    render cheese-list comp & switch logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Cheese.svg';
 import './App.css';
 import CheeseList from './components/cheese-list';
 
